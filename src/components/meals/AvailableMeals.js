fb9aa11d349ce99bcdef4b70c28a3c2b9972e7ff
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-09-28 18:46:42 +0530

    Display the meals

--- src/components/meals/AvailableMeals.js
+++ src/components/meals/AvailableMeals.js
@@ -1,38 +1,42 @@
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 import classes from "../css/AvailableMeals.module.css";
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
 
 const AvailableMeals = () => {
-  const [meals,setMeals] = useState([])
-  const [isLoading,setIsLoading] = useState(true)
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch("http://localhost:4000/meals");
       const responseData = await response.json();
-    
-      const loadMeals = [];
-      for (const key in responseData){
-        loadMeals.push({
-          id: key,
-          name: responseData[key].name,
-          description: responseData[key].description,
-          price: responseData[key].price
-        })
-      }
-      setMeals(loadMeals)
-      setIsLoading(false)
+
+      // Assuming responseData is an array with a single object
+      const mealsData = responseData[0]?.meals || {}; // Access "meals" property
+
+      const loadMeals = Object.keys(mealsData).map((mealId) => ({
+        id: mealId,
+        name: mealsData[mealId].name,
+        description: mealsData[mealId].description,
+        price: mealsData[mealId].price,
+      }));
+
+      setMeals(loadMeals);
+      setIsLoading(false);
     };
 
     fetchMeals();
   }, []);
+
   if (isLoading) {
     return <section className={classes.mealsloading}><p>Loading Please Be Patient</p></section>
   }
+
   const mealsList = meals.map((meal) => (
     <MealItem
-      id={meal.id}
       key={meal.id}
+      id={meal.id}
       name={meal.name}
       description={meal.description}
       price={meal.price}
